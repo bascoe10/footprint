62f7f5a0a7a529019c92da08b0488d4056ea6f9e
Sesay, Abass <bassco10@gmail.com>
2019-07-10 22:09:06 -0400

    fixed test import

--- tests/temp_test.py
+++ tests/temp_test.py
@@ -1,1 +1,10 @@
+import os, sys
+print(os.path.abspath(os.path.dirname(__file__)))
 from .context import footprint
+import footprint.cli
+
+import pytest, inspect
+
+def test_footprint_import():
+    assert inspect.isclass(footprint.cli.FootPrint), 'FootPrint class is available in cli'
+
