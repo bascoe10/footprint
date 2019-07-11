39ccc4c5504319a29442fb32bb169cb522ef13b7
Sesay, Abass <bassco10@gmail.com>
2019-07-11 19:19:22 -0400

    add test for bare repo

--- tests/temp_test.py
+++ tests/temp_test.py
@@ -8,6 +8,10 @@ import pytest, inspect
 def test_footprint_import():
     assert inspect.isclass(FootPrint), 'FootPrint class is available in cli'
 
+def test_footprint_non_git_repo():
+    with pytest.raises(InvalidGitRepositoryError):
+        FootPrint('./tests/non_git_repo', [], './non_git_repo')
+
 def test_footprint_with_bare_repo():
     with pytest.raises(AttributeError):
         FootPrint('./tests/test_repo.git', [], './test_repo.git')
