e43f2803d4f904d74c4686e26018f1526a332d0d
Sesay, Abass <bassco10@gmail.com>
2019-07-10 22:35:22 -0400

    test for bare repo

--- tests/temp_test.py
+++ tests/temp_test.py
@@ -9,4 +9,5 @@ def test_footprint_import():
     assert inspect.isclass(FootPrint), 'FootPrint class is available in cli'
 
 def test_footprint_with_bare_repo():
-
+    with pytest.raises(AttributeError):
+        FootPrint('./tests/test_repo.git', [], './test_repo.git')
